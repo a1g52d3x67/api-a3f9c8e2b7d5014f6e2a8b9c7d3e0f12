237e3244664739fbd3044db0059b9110d5e1d92f
a1g52d3x67 <dcordovez22@gmail.com>
2026-02-17 01:09:37 +0000

    Create Powerplan.cmd

---
+++ Powerplan.cmd
@@ -0,0 +1,16 @@
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+powercfg -setactive SCHEME_MIN
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN 5
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 99
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PERFBOOSTMODE 2
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR SYSCOOLPOL 1
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 1
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PCIEXPRESS ASPM 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_USB USBSELECTIVE 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_DISK DISKIDLE 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_NETPOWERSETTING NETWORKADAPTERPOWER 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_SLEEP STANDBYIDLE 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_SLEEP HIBERNATEIDLE 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_SLEEP HYBRIDSLEEP 0
+powercfg -changename SCHEME_CURRENT "Xploit Optimizer (XPLT v1)"
+powercfg -setactive SCHEME_CURRENT
